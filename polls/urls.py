232ccf62164698661c419ba4f2bba95c99592a27
Jeffrey A. Clark <aclark@aclark.net>
2025-01-12 15:16:05 -0500

    Writing your first Django app, part 4

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,7 @@ app_name = "polls"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<str:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<str:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<str:question_id>/vote/", views.vote, name="vote"),
 ]
